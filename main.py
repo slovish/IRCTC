1cdd353ef8b7d1722315ad00ba6b476f4a73206b
slovish <vishalkrishnajha2001@gmail.com>
2023-07-10 10:54:32 +0530

    2nd commit

--- main.py
+++ main.py
@@ -64,7 +64,6 @@ def signup(user: Userbase, db:user_db_dependency):
         "user_id": user.user_id
     }
 
-# User Login
 @app.post("/api/login")
 def login(username: str, password: str, db:user_db_dependency):
     user_data = db.query(Userbase).filter(Userbase.username == username, Userbase.password == password).first()
@@ -79,7 +78,6 @@ def login(username: str, password: str, db:user_db_dependency):
     else:
         raise HTTPException(status_code=401, detail="Incorrect username/password provided")
 
-# Add a New Train (Admin operation)
 @app.post("/api/trains/create")
 def create_train(train: Trainbase, db: user_db_dependency):
     train = Train
@@ -91,41 +89,10 @@ def create_train(train: Trainbase, db: user_db_dependency):
         "train_id": train.train_id
     }
 
-# Get Seat Availability
 @app.get("/api/trains/availability")
 def get_seat_availability(source: str, destination: str, db: user_db_dependency):
     train_data = db.query(Trainbase).filter(Trainbase.source == source, Trainbase.destination == destination).all()
     return train_data
-
-# # Book a Seat
-# @app.post("/api/bookings")
-# def book_seat(train_id: str, seats: int, authorized: bool = Depends(get_api_key)):
-#     for train in trains_db:
-#         if train.train_id == train_id:
-#             booked_seats = sum(booking.seats_booked for booking in bookings_db if booking.train_id == train_id)
-#             available_seats = train.seat_capacity - booked_seats
-#             if available_seats >= seats:
-#                 # Create booking record
-#                 user_id = "12345"  # Get the user_id from the authenticated user
-#                 booking_id = str(len(bookings_db) + 1)  # Generate unique booking ID
-#                 booking = Booking(booking_id=booking_id, user_id=user_id, train_id=train_id, seats_booked=seats)
-#                 bookings_db.append(booking)
-#                 return {
-#                     "status": "Booking successful",
-#                     "status_code": 200,
-#                     "booking_id": booking_id
-#                 }
-#             else:
-#                 raise HTTPException(status_code=400, detail="Insufficient seats available")
-#     raise HTTPException(status_code=404, detail="Train not found")
-
-# # Get Specific Booking Details
-# @app.get("/api/bookings/{booking_id}")
-# def get_booking_details(booking_id: str, authorized: bool = Depends(get_api_key)):
-#     for booking in bookings_db:
-#         if booking.booking_id == booking_id:
-#             return booking
-#     raise HTTPException(status_code=404, detail="Booking not found")
 
 
 if __name__ == "__main__":
